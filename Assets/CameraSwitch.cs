0c25623f62f5de59ccc24de2d8bded6f319a919f
rockpower <spamera001@gmail.com>
2016-09-21 16:05:30 -0700

    Rick's commit1

--- Assets/CameraSwitch.cs
+++ Assets/CameraSwitch.cs
@@ -5,10 +5,13 @@ public class CameraSwitch : MonoBehaviour {
 
     public GameObject Cam1;
     public GameObject Cam2;
+    public bool camSwitch;
 
     // Update is called once per frame
-    void Update () {
-	    if (Input.GetKeyDown(KeyCode.Keypad1))
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             if (Cam1.active == false)
             {
@@ -24,6 +27,24 @@ public class CameraSwitch : MonoBehaviour {
                 Cam1.SetActive(false);
                 Cam2.SetActive(true);
             }
+        }
+        */
+        if (camSwitch)
+        {
+            Cam1.SetActive(true);
+            Cam2.SetActive(false);
+        }
+
+        if (!camSwitch)
+        {
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("SwitchView"))
+        {
+            camSwitch = !camSwitch;
+            Debug.Log("Pressed SwitchView");
         }
     }
 }
--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -25,29 +25,37 @@ public class CharMovement : MonoBehaviour {
         //if (!animator.GetBool("Attack1Trigger"))
         if (!animator.GetCurrentAnimatorStateInfo(0).IsName("attack 1"))
         {
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle") | animator.GetBool("Running") | animator.GetBool("Sprinting"))
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("attack 2"))
             {
-                //Running
-                Vector3 pos = transform.position;
-                pos.z += Input.GetAxis("Vertical") * runSpeed * Time.deltaTime;
-                pos.x += Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime;
-                if (animator.GetCurrentAnimatorStateInfo(0).IsName("sprint"))
+                if (!animator.GetCurrentAnimatorStateInfo(0).IsName("attack 5"))
                 {
-                    pos.z += Input.GetAxis("Vertical") * addSprintSpeed * Time.deltaTime;
-                    pos.x += Input.GetAxis("Horizontal") * addSprintSpeed * Time.deltaTime;
-                }
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle") | animator.GetBool("Running") | animator.GetBool("Sprinting"))
+                    {
+                        //Running
+                        Vector3 pos = transform.position;
+                        pos.z += Input.GetAxis("Vertical") * runSpeed * Time.deltaTime;
+                        pos.x += Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime;
+                        if (animator.GetCurrentAnimatorStateInfo(0).IsName("sprint"))
+                        {
+                            pos.z += Input.GetAxis("Vertical") * addSprintSpeed * Time.deltaTime;
+                            pos.x += Input.GetAxis("Horizontal") * addSprintSpeed * Time.deltaTime;
+                        }
 
-                //Jumping
-                if (Input.GetButtonDown("Jump"))
-                {
-                    Debug.Log("Jump button");
-                    animator.SetTrigger("JumpTrigger");
-                    //animator.SetBool("InAir", true);
-                    //pos.y = transform.Translate(Vector3.up * jumpAmount* Time.deltaTime, Space.World);
-                }
+                        //Jumping
+                        if (Input.GetButtonDown("Jump"))
+                        {
+                            Debug.Log("Jump button");
+                            animator.SetTrigger("JumpTrigger");
+                            //animator.SetBool("InAir", true);
+                            //pos.y = transform.Translate(Vector3.up * jumpAmount* Time.deltaTime, Space.World);
+                        }
 
-                transform.position = pos;
+                        transform.position = pos;
+                    }
+                }
             }
+
+            
         }
     }
 }
